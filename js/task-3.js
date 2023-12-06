06e8c828a7f23cb6f49eb292b89df1f166465ca8
Oleh Povorozniuk <oleh.povoroznuik@gmail.com>
2023-12-06 22:45:31 +0200

    ...s

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,12 @@
 'use strict'
-function etElementWidth(content, padding, border){
-    const sumWeight = content + (padding * 2) + (border * 2);
+function getElementWidth(content, padding, border){
+    const nulContent = parseFloat(content);
+    const nulPadding = parseFloat(padding);
+    const nulBorder = parseFloat(border);
+    const sumWeight = nulContent + (nulPadding * 2) + (nulBorder * 2);
     return sumWeight;
 }
-console.log(etElementWidth(60, 12, 8.5));
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
